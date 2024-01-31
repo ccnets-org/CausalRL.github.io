7bb87c2c5224975e6276fbedc31088f32d3718e7
JeongYoong Kim <95277008+Ryoongyoong@users.noreply.github.com>
2024-01-31 14:40:12 +0900

    Update Side Bar Figure

---
+++ js_funcs.js
@@ -0,0 +1,8 @@
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
